e9ed133fcde5298a941bebb774de6eebb945ff52
Juan Osorio <juan.michael.osorio@gmail.com>
2020-04-19 18:24:52 -0500

    changed confirm and prompt text

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 //function that generates the random password
 function passwordGen() {
   var characters = genPossibleCharacters(possibleCharacters);
-  var length = genPasswordLength(passwordLength); 
+  var length = genPasswordLength(passwordLength);
   var result = '';
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
@@ -16,21 +16,21 @@ var possibleCharacters = '';
 //prompts that add the characters to be used for the password to possibleCharacters
 function genPossibleCharacters(possibleCharacters) {
   while (possibleCharacters === '') {
-    var numCrit = confirm('Use numbers in password?');
+    var numCrit = confirm('Do you want to use numbers in your password?');
     if (numCrit === true) {
       possibleCharacters = possibleCharacters + '0123456789';
     }
-    var capCrit = confirm('Use Capital letters in password?');
+    var capCrit = confirm('Do you want to use Capital letters in your password?');
     if (capCrit === true) {
       possibleCharacters = possibleCharacters + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
-    var lowCrit = confirm('Use Lowercase letters in password?');
+    var lowCrit = confirm('Do you want to use lowercase letters in your password?');
     if (lowCrit === true) {
       possibleCharacters = possibleCharacters + 'abcdefghijklmnopqrstuvwxyz';
     }
-    var specCrit = confirm('Use Special characters in password?');
+    var specCrit = confirm('Do you want to use special characters in password?');
     if (specCrit === true) {
-      possibleCharacters = possibleCharacters + '\!\%\*\@\$\#\^\*\&\'\(\)\+\-\;\:\<\ \.\/\=\>\?\[\]\_\`\{\|\}\~\\';
+      possibleCharacters = possibleCharacters + '\!\%\*\@\$\#\^\*\&\'\(\)\+\-\;\:\<\.\/\=\>\?\[\]\_\`\{\|\}\~\\';
     }
   }
   return possibleCharacters;
@@ -41,7 +41,7 @@ var passwordLength = 0
 //Prompts user to select a length within 8 and 128
 function genPasswordLength(passwordLength) {
   while (passwordLength < 8 || passwordLength > 128) {
-    var passwordLength = prompt('Please choose the length of your password between 8 and 128');
+    var passwordLength = prompt('Please choose the length of your password between 8 and 128 characters in Length');
   }
   return passwordLength;
 }
@@ -49,10 +49,6 @@ function genPasswordLength(passwordLength) {
 
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
-//passwordText.value = passwordGen();  <--- better?
-
-
-
 
 // Add event listener to generate button
 
